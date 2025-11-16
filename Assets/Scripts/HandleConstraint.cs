eaabef2f35857dfc4a96e2dc87b558eac2d39a0f
AKSB-GP <alexander.kazakov1999@gmail.com>
2025-11-16 18:56:34 +0100

    more exp

---
+++ Assets/Scripts/HandleConstraint.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+using Unity.Mathematics;
+
+
+public class HandleConstraint : MonoBehaviour
+{
+    public Transform startPoint;
+    public Transform endPoint;
+    public Transform parentObject; // Add reference to parent
+    private Vector3 restLocalPosition;
+    private quaternion restRotation;
+    private Vector3 movementDistance;
+    private bool isGrabbed = false;
+    public InputAction FiringValue;
+
+    void Start()
+    {
+        restLocalPosition = transform.localPosition;
+        restRotation = transform.localRotation;
+        movementDistance = endPoint.position - startPoint.position;
+        Debug.DrawLine(Vector3.zero, new Vector3(0, 5, 0), Color.red);
+        if (parentObject == null)
+        {
+            parentObject = transform.parent;
+        }
+    }
+    void OnEnable()
+    {
+        FiringValue.Enable();
+    }
+
+    void OnDisable()
+    {
+        FiringValue.Disable();
+    }
+
+
+    public void OnGrabEntered(SelectEnterEventArgs args)
+    {
+        Transform hand = args.interactorObject.transform;
+        Transform handle = args.interactableObject.transform;
+        Vector3 handleLocalPos = handle.localPosition;
+        Vector3 value = Vector3.Project(movementDistance, movementDistance);
+        
+        handleLocalPos.x = Mathf.Clamp(value.x,startPoint.localPosition.x,endPoint.localPosition.x);
+
+    }
+    
+    public void OnGrabExited(SelectExitEventArgs args)
+    {
+        // Return to rest position in local space
+        transform.localPosition = restLocalPosition;
+        transform.localRotation = restRotation;
+    }
+    
+    
+
+}
