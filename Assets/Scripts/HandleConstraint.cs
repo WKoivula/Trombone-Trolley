442325f46f73a4fb30296bfa9f84224bf0707eaf
AKSB-GP <alexander.kazakov1999@gmail.com>
2025-11-22 21:34:38 +0100

    saved prog

--- Assets/Scripts/HandleConstraint.cs
+++ Assets/Scripts/HandleConstraint.cs
@@ -3,11 +3,14 @@ using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.InputSystem;
 using Unity.Mathematics;
 using System;
+using System.Numerics;
+using Vector3 = UnityEngine.Vector3;
 
 
 public class HandleConstraint : MonoBehaviour
 {
     public bool debugOn;
+    public BoxCollider interactionZone;
     public Transform startPoint;
     public Transform endPoint;
     public Transform parentObject; // Add reference to parent
@@ -17,17 +20,24 @@ public class HandleConstraint : MonoBehaviour
     private Vector3 movementDistance;
     public bool isDebug = false;
     public InputAction FiringValue;
+
+    public Transform hand; 
     Transform currentGrabber;
     Transform handleTransform;
+    private Vector3 handleTransform_local;
     Vector3 handleLocalStartPos;
-
+    private bool isGrabbed;
     void Start()
     {
         restLocalPosition = transform.localPosition;
         handleTransform = transform;
+        handleTransform_local = transform.localPosition;
         restRotation = transform.localRotation;
-        Debug.DrawRay(startPoint.position,endPoint.position-startPoint.position,Color.red,60f);
-
+        Debug.DrawRay(startPoint.position, endPoint.position - startPoint.position, Color.red, 60f);
+        if (interactionZone == null)
+        {
+            Debug.Log("No box collider was added, did u forget?");
+        }
     }
     /*  */
 
@@ -39,29 +49,48 @@ public class HandleConstraint : MonoBehaviour
     private void exitGrab()
     {
         currentGrabber = null;
- 
+
     }
+
+
+
     //move logic to update
     void Update()
     {
-            if(debugOn) Debug.DrawRay(startPoint.position,endPoint.position-startPoint.position,Color.red,60f);
-        while ( currentgrabber != null)
+        if (debugOn) Debug.DrawRay(startPoint.position, endPoint.position - startPoint.position, Color.red, 60f);
+        if (currentGrabber != null)
         {
-            handleLocalStartPos
+
+
+
         }
+//* Time.deltaTime* 0.01f
+        float t = Mathf.Sin(-hand.localPosition.z * 0.01f );
+        Vector3 dir = (endPoint.position - startPoint.position).normalized;
+        transform.localPosition = new Vector3(t, 0, 0);
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public void OnGrabEntered(SelectEnterEventArgs args)
     {
         onGrab(args.interactorObject.transform);
-        Vector3 handleLocalPos = transform.localPosition;
-        Vector3 handLocalSpace = transform.InverseTransformPoint(args.interactorObject.transform.parent.position);
-        
-        handleLocalPos.x = Mathf.Lerp(handLocalSpace.x,startPoint.position.x,endPoint.position.x);
-        handleLocalPos.y = (endPoint.position.y-startPoint.position.y);
-        Debug.Log(args.interactableObject.transform.parent); // null
-        Debug.Log(args.interactorObject.transform.parent); // hand
-        Debug.Log(transform); //handle om bara transform 
+
     }
 
     public void OnGrabExited(SelectExitEventArgs args)
