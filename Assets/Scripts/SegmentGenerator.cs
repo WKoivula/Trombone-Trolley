5a885cd6d0191932ff4a25bdae3325b659f2b4a9
Andreas Garcia Hamrin <andreasgarciia2000@gmail.com>
2025-11-23 20:32:21 +0100

    feat: trigger based segment spawning and more smooth infinity runner effect

--- Assets/Scripts/SegmentGenerator.cs
+++ Assets/Scripts/SegmentGenerator.cs
@@ -1,29 +1,48 @@
-using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class SegmentGenerator : MonoBehaviour {
+public class SegmentGenerator : MonoBehaviour
+{
+    [Header("Setup")]
+    public GameObject[] segmentPrefabs;
+    public float segmentLength = 50f;
+    public int maxSegments = 4;
 
+    [Header("Debug")]
+    public float nextXPos = -50f;
 
-    public GameObject[] segment;
+    private readonly List<GameObject> activeSegments = new List<GameObject>();
 
-    [SerializeField] int xPos = -50;
-    [SerializeField] bool creatingSegment = false;
-    [SerializeField] int segmentNum;
-    void Update() {
-
-        if(creatingSegment == false) {
-            creatingSegment = true;
-            StartCoroutine(SegmentGen());
-        } 
+    void Start()
+    {
+        for (int i = 0; i < maxSegments-1; i++)
+        {
+            SpawnSegment();
+        }
     }
-    
-    IEnumerator SegmentGen() {
-        segmentNum = Random.Range(0, 1); // In case we want to add more segments later
-        GameObject clone = Instantiate(segment[segmentNum], new Vector3( xPos, 0, 0), Quaternion.identity);
-        Destroy(clone, 30f);
-        xPos -= 50;
-        yield return new WaitForSeconds(10);
-        creatingSegment = false;
+
+    public void SpawnSegment()
+    {
+        int index = Random.Range(0, segmentPrefabs.Length); 
+        GameObject newSeg = Instantiate(
+            segmentPrefabs[index],
+            new Vector3(nextXPos, 0f, 0f),
+            Quaternion.identity
+        );
+
+        activeSegments.Add(newSeg);
+        nextXPos -= segmentLength; 
     }
-    
+
+    public void OnSegmentPassed(GameObject segment)
+    {
+        SpawnSegment();
+
+        if (activeSegments.Count > maxSegments)
+        {
+            GameObject oldest = activeSegments[0];
+            activeSegments.RemoveAt(0);
+            Destroy(oldest);
+        }
+    }
 }
