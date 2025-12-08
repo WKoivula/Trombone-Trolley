e4ad045c1018e5659217f079300ab98a60182df6
AKSB-GP <alexander.kazakov1999@gmail.com>
2025-12-08 16:07:37 +0100

    Feature: vr ui

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using Unity.Mathematics;
+public class GameManager : MonoBehaviour
+{
+    public static GameManager _instance;
+    public static GameManager Instance { get { return _instance; } }
+    public enum GameState { Start, GameOver, Playing }
+    public GameState currentState;
+    
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else if (_instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+    }
+    
+    void Start()
+    {
+        currentState = GameState.Start;        
+        
+    }
+    public void StartGame()
+    {
+        currentState = GameState.Playing;
+    }
+    public void EndGame()
+    {
+        currentState = GameState.GameOver;
+     
+    }
+ 
+}
