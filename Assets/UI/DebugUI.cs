f7b9715d24f6d455c9808cf8706befa51462073a
AKSB-GP <alexander.kazakov1999@gmail.com>
2025-12-08 12:07:19 +0100

    added distancemeasure script

--- Assets/UI/DebugUI.cs
+++ Assets/UI/DebugUI.cs
@@ -6,7 +6,10 @@ public class DebugUI : MonoBehaviour
     [SerializeField] public Canvas canvas;
     [SerializeField] public TMP_Text currentSpeedText;
     [SerializeField] public TMP_Text newSpeedText;
+    [SerializeField] public TMP_Text distanceText;
     [SerializeField] public CartMovement cartClass;
+    [SerializeField] public DistanceHandler distanceHandlerClass;
+
     public bool DebugActive;
     LineRenderer lineRenderer;
 
@@ -22,9 +25,11 @@ public class DebugUI : MonoBehaviour
     {
         if (DebugActive)
         {
+            distanceHandlerClass.
             currentSpeedText.text = $"{cartClass.currentSpeed}";
             newSpeedText.text = $"{cartClass.newSpeed}";
         }
+        distanceText.text = $"Distance: {distanceHandlerClass.distanceTraveled}";
     }
 
 }
